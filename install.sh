6f68b50ea0e318dd3fa15078e44f708182a3215a
WorkOfArtiz <arghhhhhthur@gmail.com>
2016-12-27 00:19:01 +0100

    added installers for scripts

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# 88888888888          888
+#     888              888
+#     888              888
+#     888  .d88b.  .d88888 .d88b.
+#     888 d88""88bd88" 888d88""88b
+#     888 888  888888  888888  888
+#     888 Y88..88PY88b 888Y88..88P
+#     888  "Y88P"  "Y88888 "Y88P"
+#
+#    This script is part of the Todo program
+#    Arthur de Fluiter 26 Dec 2k16
+chmod 777 todo*
+sudo cp todo* /usr/bin/
+
+read -p "Do you also want to install lolcat/figlet [colours and big letters]" -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo apt-get install lolcat figlet -y
+fi
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo rm -f /usr/bin/todo /usr/bin/todo-list /usr/bin/todo-remove /usr/bin/todo-clear
